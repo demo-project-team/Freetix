9ec651cd927d27fdb09e41c3a0b099af21d0392a
24LP4462 24LP4462 <24LP4462@G301-CC0194.local>
2025-04-24 21:03:11 +0800

    header oorcholt

--- frontend/src/components/Gameorder.tsx
+++ frontend/src/components/Gameorder.tsx
@@ -7,12 +7,6 @@ import { ChevronDown } from "lucide-react";
 import { getCategory } from "@/utils/request/categoryRequest";
 import { useQuery } from "@tanstack/react-query";
 
-type Category = {
-    name: string;
-    id: string;
-    icon: string;
-  };
-
 export const GameOrder = () => {
     const { data: category = [] } = useQuery({
         queryKey: ["category"],
@@ -24,7 +18,7 @@ export const GameOrder = () => {
         <ChevronDown/>
       </PopoverTrigger>
       <PopoverContent>
-        {category.map((category:Category, index) => (
+        {category.map((category, index) => (
             <div key={index} className="flex w-[260px] justify-center items-center mt-[10px]">
                 <div className="flex h-[60px] items-center gap-2">
                     <p>{category.name}</p>
