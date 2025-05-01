886f0af60427cef58715d0e3bbcc57679aec1060
24LP4462 24LP4462 <24LP4462@G301-CC0194.local>
2025-05-01 17:11:31 +0800

    logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+
 import { useRouter } from "next/navigation";
 import { OpenUser } from "./Openuser";
 import { useUser } from "@/provider/UserProvider";
 import { User } from "lucide-react";
+import { motion } from "framer-motion";
 
 const Header = () => {
   const router = useRouter();
@@ -11,15 +13,28 @@ const Header = () => {
   console.log(user);
   return (
     <header className="sticky top-0 left-0 right-0 z-50 flex justify-between items-center p-6 bg-black bg-opacity-50 backdrop-blur-md">
-      <div className="flex items-center space-x-2">
-        <span className="text-3xl">🎮</span>
-        <span
-          className="text-3xl font-bold bg-gradient-to-r from-blue-400 to-indigo-500 bg-clip-text text-transparent"
-          onClick={() => router.push("/")}
-        >
-          eSlot
-        </span>
-      </div>
+       <motion.div
+      className="flex items-center space-x-2 cursor-pointer"
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      onClick={() => router.push("/")}
+    >
+      <motion.span
+        className="text-4xl"
+        animate={{ rotate: [0, 20, -20, 0] }}
+        transition={{ repeat: Infinity, duration: 2, ease: "easeInOut" }}
+      >
+        🎮
+      </motion.span>
+      <motion.span
+        className="text-4xl font-extrabold bg-gradient-to-r from-cyan-400 via-blue-500 to-purple-600 bg-clip-text text-transparent drop-shadow-lg"
+        whileHover={{ scale: 1.1 }}
+        transition={{ type: "spring", stiffness: 300 }}
+      >
+        eSlot
+      </motion.span>
+    </motion.div>
       <div className="hidden md:flex space-x-6 text-lg">
         {user ? (
           <div className="flex items-center justify-center">
@@ -29,6 +44,7 @@ const Header = () => {
           <OpenUser />
         )}
       </div>
+
       <div className="md:hidden flex items-center space-x-4">
         <a href="#login" className="text-blue-400">
           Нэвтрэх
