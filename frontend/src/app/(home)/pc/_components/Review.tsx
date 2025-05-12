f0f712f4a5fed2e29ee20a4820854640f057e4ff
24LP4462 24LP4462 <24LP4462@G301-CC0194.local>
2025-05-12 15:27:25 +0800

    RatingFilter

--- frontend/src/app/(home)/pc/_components/Review.tsx
+++ frontend/src/app/(home)/pc/_components/Review.tsx
@@ -5,76 +5,97 @@ import {
   Dialog,
   DialogContent,
   DialogHeader,
+  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Vendor } from "@/Types/types";
-import { User } from "lucide-react";
+import { Star, User } from "lucide-react";
+import React, { useMemo, useState } from "react";
+import { formatDistanceToNow } from "date-fns";
 
 export const Review = ({ vendor }: { vendor: Vendor }) => {
-  //   const renderStars = (ratingAvg) => {
-  //     return (
-  //       <div className="flex">
-  //         {[...Array(5)].map((_, i) => (
-  //           <Star
-  //             key={i}
-  //             size={16}
-  //             fill={i < ratingAvg ? "#FFD700" : "none"}
-  //             color={i < ratingAvg ? "#FFD700" : "#D1D5DB"}
-  //           />
-  //         ))}
-  //       </div>
-  //     );
-  //   };
+  const renderStars = (rating: number) => {
+    return (
+      <div className="flex">
+        {[...Array(5)].map((_, i) => (
+          <Star
+            key={i}
+            size={16}
+            fill={i < rating ? "#FFD700" : "none"}
+            color={i < rating ? "#FFD700" : "#D1D5DB"}
+          />
+        ))}
+      </div>
+    );
+  };
+  const [sortOrder, setSortOrder] = useState<"highest" | "lowest">("highest");
+
+  const SortReviews = useMemo(() => {
+    return [...vendor.reviews].sort((a, b) =>
+      sortOrder === "highest" ? b.rating - a.rating : a.rating - b.rating
+    );
+  }, [vendor.reviews, sortOrder]);
+
+  const handleRatingFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setSortOrder(value === "Highest rated" ? "highest" : "lowest");
+  };
+
   return (
     <Dialog>
       <DialogTrigger className="bg-white px-2 py-3 rounded-xl">
         Open
       </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
-          <div className="grid gap-6">
+        <div className="grid gap-6">
+          <DialogHeader>
             <div className="flex items-center justify-between mb-6">
-              <h2 className="text-2xl font-bold">11 reviews</h2>
-              <select className="border rounded-full px-4 py-2">
-                <option>Most recent</option>
+              <DialogTitle>{vendor.reviews.length} reviews</DialogTitle>
+
+              <select
+                className="border rounded-full px-4 py-2"
+                onChange={handleRatingFilter}
+                value={
+                  sortOrder === "highest" ? "Highest rated" : "Lowest rated"
+                }
+              >
+                <option>Lowest rated</option>
                 <option>Highest rated</option>
               </select>
             </div>
-            {vendor.reviews.map((review) => (
-              <Dialog key={review.id}>
-                <DialogTrigger asChild>
-                  <div className="bg-white rounded-xl p-6 shadow-sm hover:shadow-md transition-shadow cursor-pointer border">
-                    <div className="flex items-center gap-4 mb-3">
-                      {review.user.profileImage ? (
-                        <img
-                          src={review.user.profileImage}
-                          className="w-12 h-12 bg-gray-200 rounded-full"
-                        />
-                      ) : (
-                        <User stroke="black"/>
-                      )}
+          </DialogHeader>
+          {SortReviews.map((review) => (
+            <div
+              key={review.id}
+              className="bg-white rounded-xl p-6 shadow-sm hover:shadow-md transition-shadow cursor-pointer border"
+            >
+              <div className="flex items-center gap-4 mb-3">
+                {review.user.profileImage ? (
+                  <img
+                    src={review.user.profileImage}
+                    className="w-12 h-12 bg-gray-200 rounded-full"
+                  />
+                ) : (
+                  <User stroke="black" />
+                )}
 
-                      <div>
-                        <h3 className="font-medium text-black">
-                          {review.user.name}
-                        </h3>
-                      </div>
-                    </div>
-                    <div className="flex items-center gap-2 mb-2">
-                      <p className="text-black">{review.rating}</p>
-                      <p className="text-black text-sm">
-                        • {review.createdAt.toString().split("T")[0]}
-                      </p>
-                    </div>
-                    <p className="line-clamp-3 text-gray-700">
-                      {review.comment}
-                    </p>
-                  </div>
-                </DialogTrigger>
-              </Dialog>
-            ))}
-          </div>
-        </DialogHeader>
+                <div>
+                  <h3 className="font-medium text-black">{review.user.name}</h3>
+                </div>
+              </div>
+              <div className="flex items-center gap-2 mb-2">
+                <p className="text-black">{renderStars(review.rating)}</p>
+                <p className="text-black text-sm">
+                  •
+                  {formatDistanceToNow(new Date(review.createdAt), {
+                    addSuffix: true,
+                  })}
+                </p>
+              </div>
+              <p className="line-clamp-3 text-gray-700">{review.comment}</p>
+            </div>
+          ))}
+        </div>
       </DialogContent>
     </Dialog>
   );
